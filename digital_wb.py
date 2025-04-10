f34ff01b3aab53d7c0970b06831d2f10db3a4fc1
Michael Pettinger <pettinger.michael@gmail.com>
2025-04-10 12:25:51 -0500

    Added calibration and get weight functions.

--- digital_wb.py
+++ digital_wb.py
@@ -1,7 +1,11 @@
 from scale import scale
 import time
-s1 = scale(3,5)
-while True:
-    print(s1.get_avg_raw(n=100))
-    time.sleep(.500)
+s1 = scale(3.1,5)
+s1.calibrate()
+continue_ = "GO"
+while continue_ != "Q":
+    continue_ = input("Enter Q to stop: ")
+    for i in range(5):
+        print(s1.get_weight())
+        time.sleep(.500)
 s1.clean_up()
--- scale.py
+++ scale.py
@@ -1,11 +1,19 @@
 import RPi.GPIO as GPIO
-import time
 import numpy as np
 
 class scale:
-    def __init__(self,clk,data):
+    def __init__(self,clk,data,name=None):
+        assert isinstance(clk,int), f"Clock pin must be an integer"
+        assert isinstance(data,int), f"Data pin must be an integer"
         self.clk = clk
         self.data = data
+        self.intercept = 0
+        self.slope = 0
+        self.tare = 0
+        if name == None:
+            self.name = f"Scale on pin {self.data}"
+        if name != None:
+            self.name = name
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(clk,GPIO.OUT)
         GPIO.setup(data,GPIO.IN)
@@ -26,18 +34,29 @@ class scale:
         return my_data
     
     def get_avg_raw(self,n=10):
-        samples = np.array()
-        while samples.std() > 500:
+        samples = []
         for i in range(n):
             samples.append(self.get_raw_reading())
-        samples = np.array(samples)
-        return (samples.mean(),samples.std())
+        samples_np = np.array(samples)
+        return (samples_np.mean(),samples_np.std())
     
     def calibrate(self):
         input("Make sure scale is empty and press enter.")
         low_weight = 0
+        reading_low = self.get_avg_raw()[0]
         high_weight = float(input("Load scale and enter known weight in Kgs: "))
-        slope = (low_weight - high_weight)/
+        reading_high = self.get_avg_raw()[0]
+        
+        slope = (low_weight - high_weight)/(reading_low-reading_high)
+        intercept = low_weight - slope*reading_low
+        
+        self.slope = slope
+        self.intercept = intercept
     
+    def get_weight(self):
+        reading = self.get_avg_raw()
+        weight = self.slope*reading[0] + self.intercept
+        return weight - self.tare
+            
     def clean_up(self):
         GPIO.cleanup([self.clk,self.data])
